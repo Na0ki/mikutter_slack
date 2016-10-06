4443a5ec2aa67a747dac40af4184a8638ba681b4
ahiru <wellingtonnmaeda0524@gmail.com>
2016-10-06 12:49:12 +0900

    投稿内容が空の場合は無視する URLを投稿した際にSlack側で展開されることがあるが、展開された部分は空の投稿として投げられる そのため、mikutter_slackでは空の投稿ができてしまうが、これによってそれを防止する

--- slack.rb
+++ slack.rb
@@ -67,6 +67,7 @@ Plugin.create(:slack) do
     # 起動時間より前のタイムスタンプの場合は何もしない（ヒストリからとってこれる）
     # 起動時に最新の一件の投稿が呼ばれるが、その際に on :message が呼ばれてしまうのでその対策
     next unless @defined_time < Time.at(Float(data['ts']).to_i)
+    next if data['text'].empty?
 
     # メッセージの処理
     Thread.new {
