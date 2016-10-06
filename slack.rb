32a2cdd5b6387c41530b1ead46d4295a0a415e82
ahiru <wellingtonnmaeda0524@gmail.com>
2016-10-06 16:57:00 +0900

    コメント追加

--- slack.rb
+++ slack.rb
@@ -64,6 +64,7 @@ Plugin.create(:slack) do
   RTM.on :message do |data|
     # 起動時間より前のタイムスタンプの場合は何もしない（ヒストリからとってこれる）
     # 起動時に最新の一件の投稿が呼ばれるが、その際に on :message が呼ばれてしまうのでその対策
+    # @defined_time は {https://github.com/toshia/pluggaloid/blob/master/lib/pluggaloid/plugin.rb#L96} で定義済み
     next unless @defined_time < Time.at(Float(data['ts']).to_i)
     # 投稿内容が空の場合はスキップ
     next if data['text'].empty?
