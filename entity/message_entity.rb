115b58fb895ba2fdc3776a3a3f2a06b0e1eadf1a
ahiru <wellingtonnmaeda0524@gmail.com>
2016-12-31 18:23:08 +0900

    `<@ユーザーID|ユーザー名>` を `@ユーザー名` に置換

--- entity/message_entity.rb
+++ entity/message_entity.rb
@@ -5,24 +5,24 @@ module Plugin::Slack
   module Entity
 
     MessageEntity = Retriever::Entity::RegexpEntity.
-        filter(/<(https:\/\/.+\.slack\.com\/files\/([\w\-]+)\/([\w\-]+)\/(.+)\.(jpg|jpeg|gif|png|bmp)(|\|(.*)))>/, generator: -> s {
-          s_url = +s[:url]
-          p "s_url = #{s_url}"
-          no_title = /<(https:\/\/.+\.slack\.com\/files\/([\w\-]+)\/(.+)\.(jpg|jpeg|gif|png|bmp))>/.match(s[:url])
-          with_title = /<(https:\/\/.+\.slack\.com\/files\/([\w\-]+)\/([\w\-]+)\/(.+)\.(jpg|jpeg|gif|png|bmp)\|(.*))>/.match(s[:url])
-          if no_title.nil?
-            uri = with_title[3] # files-priに続くURI
-            ext = with_title[4] # ファイル拡張子
-            face = with_title[5] # 表示（ファイル名）
-          else
-            uri = no_title[3] # files-priに続くURI
-            ext = no_title[4] # ファイル拡張子
-            face = no_title[1] # 表示（元URL）
-          end
-          url = "https://files.slack.com/files-pri/#{s[:message].team.id}-#{uri}.#{ext}"
-          s.merge(open: url,
-                  face: face,
-                  url: url)
+        filter(/<(https:\/\/.+\.slack\.com\/files\/([\w\-]+)\/([\w\-]+)\/([\w\-]+)\.(jpg|jpeg|gif|png|bmp)(|\|(.*)))>/, generator: -> s {
+          # s_url = +s[:url]
+          # p "s_url = #{s_url}"
+          # no_title = /<(https:\/\/.+\.slack\.com\/files\/([\w\-]+)\/([\w\-]+)\.(jpg|jpeg|gif|png|bmp))>/.match(s[:url])
+          # with_title = /<(https:\/\/.+\.slack\.com\/files\/([\w\-]+)\/([\w\-]+)\/([\w\-]+)\.(jpg|jpeg|gif|png|bmp)\|(.*))>/.match(s[:url])
+          # if no_title.nil?
+          #   uri = with_title[3] # files-priに続くURI
+          #   ext = with_title[4] # ファイル拡張子
+          #   face = with_title[5] # 表示（ファイル名）
+          # else
+          #   uri = no_title[3] # files-priに続くURI
+          #   ext = no_title[4] # ファイル拡張子
+          #   face = no_title[1] # 表示（元URL）
+          # end
+          # url = "https://files.slack.com/files-pri/#{s[:message].team.id}-#{uri}.#{ext}"
+          # s.merge(open: url,
+          #         face: face,
+          #         url: url)
           s
         }).
         filter(/<(!.+)>/, generator: -> s {
@@ -33,17 +33,19 @@ module Plugin::Slack
           # s.merge(face: default_face[3])
           s
         }).
-        filter(/<(@(U.+)\|(.+)|@(U.+))>/, generator: -> s {
-          # TODO: ユーザーリストを取得して、マッチさせて表示名をIDからスクリーンネームにする
-          no_id = /<(@(U.+))>/.match(s[:face])
-          with_id = /<(@(U.+)\|(.+))>/.match(s[:face])
-          if no_id.nil?
-            p "with_id: #{with_id}"
+        filter(/<(@(U[\w\-]+)).*?>/, generator: -> s {
+          no_name = /^(?!.*\|).*(?=@U+).*$/.match(s[:face]) # |（パイプ）を含まない文字列を取得
+          with_name = /<(@(U.+)\|(.+))>/.match(s[:face]) # |（パイプ）の後にユーザー名が入っているもの
+          if no_name.nil?
+            user_id = with_name[2]
+            user_name = "@#{with_name[3]}"
           else
-            p "no_id: #{no_id}"
+            no_name = /<(@(U.+))>/.match(s[:face])
+            user_id = no_name[1]
+            user_name = no_name[1] # FIXME: ユーザーリストを取得して、マッチさせて表示名をIDからスクリーンネームにする
           end
-          # s.merge(face: default_face[1])
-          s
+          s.merge(url: user_id,
+                  face: user_name)
         }).
         filter(/:[\w\-]+:/, generator: -> s {
           emoji_name = /:([\w\-]+):/.match(s[:face])[1]
@@ -52,7 +54,7 @@ module Plugin::Slack
                   url: 'http://totori.dip.jp/')
         }).
         filter(/<(.*)>/, generator: -> s {
-          p "others: #{s[:face]}"
+          # p "others: #{s[:face]}"
           s
         })
   end
