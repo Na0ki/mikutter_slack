bab9f1035046675a54bd1268fed17a06bdaf6d40
ahiru <wellingtonnmaeda0524@gmail.com>
2017-01-07 00:35:05 +0900

    codeの取得までは完了

--- api/auth.rb
+++ api/auth.rb
@@ -2,6 +2,7 @@
 require 'slack'
 require 'uri'
 require 'httpclient'
+require 'webrick'
 
 module Plugin::Slack
   module API
@@ -17,20 +18,37 @@ module Plugin::Slack
           client = HTTPClient.new
           query = {client_id: '43202035347.73645008566',
                    scope: 'client',
-                   redirect_uri: 'http://localhost/',
+                   redirect_uri: 'http://localhost:8080/',
                    state: 'mikutter_slack'}.to_hash
           client.get('https://slack.com/oauth/authorize', :query => query)
         }.next { |res|
-          Delayer::Deferred.fail('Not Redirect') unless (res.status_code == 302)
+          Delayer::Deferred.fail(res) unless (res.status_code == 302)
           # OAuth認証用ページへのリダイレクトURL
           oauth_redirect_uri = res.header['location'][0]
           Plugin.call(:open, "https://slack.com#{URI.decode(oauth_redirect_uri)}")
+          Thread.new {
+            http = WEBrick::HTTPServer.new({:DocumentRoot => __dir__,
+                                            :BindAddress => 'localhost',
+                                            :Port => 8080
+                                           })
+            # FIXME: WebRickでアクセストークンを取得できるようにする
+            http.mount_proc('/') do |request, response|
+              p request
+              p response
+            end
+            trap('INT') { http.stop }
+            http.start
+          }.next { |_|
+            # Delayer::Deferred.fail(response) unless (response.nil? or response&.status_code == 200)
+            p _
+          }
         }.trap { |e| error e }
       end
 
       # 認証テスト
       # @return [Delayer::Deferred::Deferredable] 認証結果を引数にcallbackするDeferred
       def auth_test
+        oauth
         Thread.new { @client.auth_test }
       end
 
