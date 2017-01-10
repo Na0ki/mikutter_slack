931f818cf0a8435d24540ad6a8e641e5e1b51a8a
ahiru <wellingtonnmaeda0524@gmail.com>
2017-01-10 12:05:53 +0900

    インスタンス変数削除

--- api/auth.rb
+++ api/auth.rb
@@ -12,11 +12,6 @@ module Plugin::Slack
     class Auth
 
       # TODO: 認証を開発者トークンとOAuthのどちらでもできるようにする
-      # バカにしか見えない…バカにしか見えない…バカにしか見えない…
-      @client_id = Plugin::Slack::Environment::SLACK_CLIENT_ID
-      @client_secret = Plugin::Slack::Environment::SLACK_CLIENT_SECRET
-      @redirect_uri = Plugin::Slack::Environment::SLACK_REDIRECT_URI
-
       def initialize(client)
         @client = client
       end
@@ -28,7 +23,12 @@ module Plugin::Slack
       def self.oauth
         Thread.new {
           client = HTTPClient.new
-          query = {client_id: @client_id, scope: 'client', redirect_uri: @redirect_uri, state: 'mikutter_slack'}.to_hash
+          query = {
+              client_id: Plugin::Slack::Environment::SLACK_CLIENT_ID,
+              scope: 'client',
+              redirect_uri: Plugin::Slack::Environment::SLACK_REDIRECT_URI,
+              state: 'mikutter_slack'
+          }.to_hash
           client.get('https://slack.com/oauth/authorize', :query => query, 'Content-Type' => 'application/json')
         }.next { |response|
           Delayer::Deferred.fail(response) unless (response.status_code == 302)
@@ -44,14 +44,13 @@ module Plugin::Slack
             @server = WEBrick::HTTPServer.new(config)
             @server.mount_proc('/') do |_, res|
               Delayer::Deferred.fail(res) unless res.status == 200
-
-              # TODO: res.request_uri を持たない場合に対応する（favicon.icoの要求）
-              # FIXME: webrickをshutdownするタイミングを修正する
               query = CGI.parse(res.request_uri.query)
               # ローカルのHTMLを表示
               res.body = open(File.join(__dir__, '../www/', 'index.html'))
+              res.content_type = 'text/html'
+              res.chunked = true
               # アクセストークンの取得
-              self.oauth_access(query['code'][0]).next { |_token|
+              self.oauth_access(query['code'][0]).next { |token|
                 @server.shutdown
               }.trap { |e| error e }
             end
@@ -83,19 +82,18 @@ module Plugin::Slack
         Thread.new(code) { |c|
           client = HTTPClient.new
           query = {
-              client_id: @client_id,
-              client_secret: @client_secret,
+              client_id: Plugin::Slack::Environment::SLACK_CLIENT_ID,
+              client_secret: Plugin::Slack::Environment::SLACK_CLIENT_SECRET,
               code: c,
-              redirect_uri: @redirect_uri
+              redirect_uri: Plugin::Slack::Environment::SLACK_REDIRECT_URI
           }.to_hash
           client.get('https://slack.com/api/oauth.access', :query => query, 'Content-Type' => 'application/json')
-        }.next { |res|
-          Delayer::Deferred.fail(res) unless res.status_code == 200
-          body = JSON.parse(res.body, symbolize_names: true)
-          Delayer::Deferred.fail(body[:error]) unless body[:ok]
-          notice "scope: #{body[:scope]}, user_id: #{body[:user_id]}, team_name: #{body[:team_name]}, team_id: #{body[:team_id]}"
-          notice "token: #{body[:access_token]}"
-          UserConfig['slack_token'] = body[:access_token]
+        }.next { |response|
+          Delayer::Deferred.fail(response) unless response.status_code == 200
+          result = JSON.parse(response.body, symbolize_names: true)
+          Delayer::Deferred.fail(result[:error]) unless result[:ok]
+          notice "scope: #{result[:scope]}, user_id: #{result[:user_id]}, team_name: #{result[:team_name]}, team_id: #{result[:team_id]}"
+          UserConfig['slack_token'] = result[:access_token]
         }
       end
 
