726e8f330dbbed28760f202c13022a23ecc01192
ahiru <wellingtonnmaeda0524@gmail.com>
2017-01-24 10:40:10 +0900

    コメント追加

--- model/channel.rb
+++ model/channel.rb
@@ -3,7 +3,7 @@
 module Plugin::Slack
 
   class Channel < Retriever::Model
-    register :slack_channel, name: "Slack Channel"
+    register :slack_channel, name: 'Slack Channel'
 
     field.string :id, required: true
     field.string :name, required: true
@@ -16,18 +16,26 @@ module Plugin::Slack
     field.int :unread_count
     field.int :unread_count_display
 
+    # 抽出タブのスラグを返す
+    # @return [String] スラグ
     def datasource_slug
       :"slack_#{team.id}_#{id}"
     end
 
+    # 抽出タブの名前を返す
+    # @return [String] 抽出タブの名前
     def datasource_name
       ['slack', team.name, name]
     end
 
+    # チャンネルのヒストリを返す
+    # @return [Delayer::Deferred::Deferredable] チャンネルの最新のMessageの配列を引数にcallbackするDeferred
     def history
       team.api.channel_history(self)
     end
 
+    # チャンネルのリンクを返す
+    # @return [String] リンク
     def perma_link
       Retriever::URI("https://#{team.domain}.slack.com/archives/#{name}/")
     end
