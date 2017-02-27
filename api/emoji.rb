7182096ca4e0100da6ba89d261a35ab132b5f882
ahiru <wellingtonnmaeda0524@gmail.com>
2017-02-27 12:15:55 +0900

    絵文字取得の準備

--- api/emoji.rb
+++ api/emoji.rb
@@ -1,19 +1,16 @@
 # -*- coding: utf-8 -*-
 require 'slack'
+require_relative 'object'
 
 module Plugin::Slack
   module API
-    class Emoji
 
-      def initialize(client)
-        @client = client
-      end
-
+    class Emoji < Object
 
       # Emojiリストの取得
       # @return [Delayer::Deferred::Deferredable] 絵文字リストを引数にcallbackするDeferred
-      def emoji_list
-        Thread.new { @client.emoji_list }
+      def list
+        Thread.new { api.client.emoji_list }
       end
     end
 
