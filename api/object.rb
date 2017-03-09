aa5a8306bcdecfea78fdfa2fa0c13ce12079a791
ahiru <wellingtonnmaeda0524@gmail.com>
2017-03-09 10:00:37 +0900

    module分離

--- api/object.rb
+++ api/object.rb
@@ -1,48 +1,50 @@
 # -*- coding: utf-8 -*-
-module Plugin::Slack::API
-  class Object
-    attr_reader :parent
-    alias_method :api, :parent
+module Plugin::Slack
+  module API
+    class Object
+      attr_reader :parent
+      alias_method :api, :parent
 
-    def initialize(parent)
-      @parent = parent
-    end
-
-    def team
-      parent.team
-    end
-
-
-    private
-
-    # TODO: comment
-    #
-    # @param [Symbol] identity
-    # @param [callback]
-    def request_thread(identity, &block)
-      promise = Delayer.Deferred.new(true)
-      request_thread_pool(identity).new do
-        begin
-          result = request_thread_cache[identity] ||= block.call
-          promise.call(result)
-        rescue Exception => err
-          promise.fail(err)
-        end
+      def initialize(parent)
+        @parent = parent
       end
-      promise
-    end
 
-    # TODO: comment
-    #
-    # @param [Symbol] _identity
-    memoize def request_thread_pool(_identity)
-      SerialThreadGroup.new
-    end
+      def team
+        parent.team
+      end
 
-    # TODO: comment
-    #
-    def request_thread_cache
-      @request_thread_cache ||= TimeLimitedStorage.new(Symbol)
+
+      private
+
+      # TODO: comment
+      #
+      # @param [Symbol] identity
+      # @param [callback]
+      def request_thread(identity, &block)
+        promise = Delayer.Deferred.new(true)
+        request_thread_pool(identity).new do
+          begin
+            result = request_thread_cache[identity] ||= block.call
+            promise.call(result)
+          rescue Exception => err
+            promise.fail(err)
+          end
+        end
+        promise
+      end
+
+      # TODO: comment
+      #
+      # @param [Symbol] _identity
+      memoize def request_thread_pool(_identity)
+        SerialThreadGroup.new
+      end
+
+      # TODO: comment
+      #
+      def request_thread_cache
+        @request_thread_cache ||= TimeLimitedStorage.new(Symbol)
+      end
     end
 
   end
